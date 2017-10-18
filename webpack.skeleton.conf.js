f020d536d46db69abeaf13d766ae367a5dc0fb4e
xiaobinwu <xiaobin_wu@yahoo.com>
2017-10-18 23:21:14 +0800

    更新npm包

--- webpack.skeleton.conf.js
+++ webpack.skeleton.conf.js
@@ -5,10 +5,11 @@
 
 const { resolve } = require('path')
 const nodeExternals = require('webpack-node-externals')
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const glob = require('glob')
 const entryJsList = {}
-const entries = glob.sync('./src/skeleton/*/entry-skeleton.js')
+const entries = glob.sync('./src/skeleton/**/entry-skeleton.js')
+console.log(entries)
 for (const path of entries) {
     const chunkName = path.slice('./src/skeleton/'.length, -'/entry-skeleton.js'.length)
     entryJsList[chunkName] = resolve(__dirname, path)
